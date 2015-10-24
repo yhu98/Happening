b1a8556b910cde17b5c69c07b5f6f4cdd14de84c
Douglas Hewitt <doughewitt4@gmail.com>
2015-10-24 02:02:59 -0700

    Successfully added Parse keys and uploaded test object

--- Happening/Happening/AppDelegate.swift
+++ Happening/Happening/AppDelegate.swift
@@ -18,6 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("fk1jsO3tN0nFCdJouIk5oJUn2wH8IRIuqwJm7Hch",
+            clientKey: "aeXTFQj1dNlYMJw0FxjbHjypuuspwZN52hHpjzfm")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        
         // Override point for customization after application launch.
         return true
     }
